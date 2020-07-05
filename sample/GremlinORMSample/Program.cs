4e0d5638148282b79eafe78775a9858d58213332
Chad Green <chadgreen@chadgreen.com>
2020-07-05 19:59:33 -0400

    Moves to new repository

---
+++ sample/GremlinORMSample/Program.cs
@@ -0,0 +1,106 @@
+﻿using GremlinORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace GremlinORMSample
+{
+
+	class Program
+	{
+
+		static async Task Main(string[] args)
+		{
+			var graphFacade = new GraphFacade(Settings.Endpoint, Settings.AuthKey, Settings.Database, Settings.Graph);
+
+			string query = "g.V().hasLabel('room').has('eventId', 10)";
+			//string query = "g.V().hasLabel('room').has('eventId', 'schemaDefinition')";
+			var results = await graphFacade.QueryAsync(query);
+
+			if (results.Any())
+			{
+				foreach (var result in results)
+				{
+					foreach (var r in result)
+					{
+						if (r.Key == "properties")
+						{
+							foreach (var v in r.Value)
+							{
+
+								var options = new JsonSerializerOptions
+								{
+									Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+								};
+
+								string[] x = SplitCsv3(JsonSerializer.Serialize(v.Value, options));
+								if (x.Length > 0)
+									for (int i = 1; i < x.Length; i++)
+									{
+										if (i % 2 != 0)
+										{
+
+											//string propertyValue = (x[i].EndsWith("}]")) ? x[i].Substring(6, x[i].Length - 8) : x[i].Substring(6, x[i].Length - 7);
+											string propertyValue = (x[i].EndsWith("}]")) ? x[i][6..^2] : x[i][6..^1];
+
+											PrintKeyValue(v.Key, propertyValue);
+
+										}
+									}
+
+							}
+						}
+						else
+						{
+							PrintKeyValue(r.Key, r.Value);
+						}
+					}
+					Console.WriteLine();
+				}
+			}
+
+		}
+
+		private static void PrintKeyValue(string key, string value)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write(key.PadRight(15));
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Write(value);
+			Console.WriteLine();
+		}
+
+		// TODO: Make this an extension method
+		public static string[] SplitCsv3(string line)
+		{
+			List<string> result = new List<string>();
+			StringBuilder currentStr = new StringBuilder("");
+			bool inQuotes = false;
+			for (int i = 0; i < line.Length; i++) // For each character
+			{
+				if (line[i] == '\"') // Quotes are closing or opening
+					inQuotes = !inQuotes;
+				else if (line[i] == ',') // Comma
+				{
+					if (!inQuotes) // If not in quotes, end of current string, add it to result
+					{
+						result.Add(currentStr.ToString());
+						currentStr.Clear();
+					}
+					else
+						currentStr.Append(line[i]); // If in quotes, just add it 
+				}
+				else // Add any other character to current string
+					currentStr.Append(line[i]);
+			}
+			result.Add(currentStr.ToString().Replace("\\u0022", "\""));
+			return result.ToArray(); // Return array of all strings
+		}
+	}
+
+}
---
+++ src/GremlinORM/GraphFacade.cs
@@ -0,0 +1,76 @@
+﻿using Gremlin.Net.Driver;
+using Gremlin.Net.Structure.IO.GraphSON;
+using System.Threading.Tasks;
+
+namespace GremlinORM
+{
+	/// <summary>
+	/// Provides access to graph related information.
+	/// </summary>
+	public class GraphFacade
+	{
+
+		/// <summary>
+		/// Gets the endpoint of the Cosmos DB account.
+		/// </summary>
+		/// <value>
+		/// A <c>string</c> that represents the Cosmos DB account endpoint.
+		/// </value>
+		public string Endpoint { get; }
+
+		/// <summary>
+		/// Gets the authentication key for the Cosmos DB account.
+		/// </summary>
+		/// <value>
+		/// A <c>string</c> representing the Cosmos DB account authentication key.
+		/// </value>
+		public string AuthKey { get; }
+
+		/// <summary>
+		/// Gets the name of the Gremlin database.
+		/// </summary>
+		/// <value>
+		/// A <c>string</c> representing the Gremlin database name.
+		/// </value>
+		public string DatabaseName { get; }
+
+		/// <summary>
+		/// Gets the name of the graph.
+		/// </summary>
+		/// <value>
+		/// A <c>string</c> representing the graph name.
+		/// </value>
+		public string GraphName { get; }
+
+		public GremlinClient GremlinClient { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GraphFacade"/> class.
+		/// </summary>
+		/// <param name="endpoint">The Cosmos DB account endpoint.</param>
+		/// <param name="authKey">The Cosmos DB account authentication key.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <param name="graphName">Name of the graph.</param>
+		public GraphFacade(string endpoint, string authKey, string databaseName, string graphName)
+		{
+			Endpoint = endpoint;
+			AuthKey = authKey;
+			DatabaseName = databaseName;
+			GraphName = graphName;
+			var gremlinServer = new GremlinServer(endpoint, 443, enableSsl: true, username: "/dbs/" + databaseName + "/colls/" + graphName, password: authKey);
+			GremlinClient = new GremlinClient(gremlinServer, new GraphSON2Reader(), new GraphSON2Writer(), GremlinClient.GraphSON2MimeType);
+		}
+
+		/// <summary>
+		/// Performs the specific query against the Gremlin server.
+		/// </summary>
+		/// <param name="gremlinQuery">The gremlin query to be executed.</param>
+		/// <returns>A <see cref="ResultSet{dynamic}"/> containing the results of the query.</returns>
+		public async Task<ResultSet<dynamic>> QueryAsync(string gremlinQuery)
+		{
+			return await GremlinClient.SubmitAsync<dynamic>(gremlinQuery).ConfigureAwait(true);
+		}
+
+	}
+
+}
